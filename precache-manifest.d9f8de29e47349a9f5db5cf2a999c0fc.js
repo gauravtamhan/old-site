528c570ba6b404329ed8c7aef5bd1eae43d88789
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2020-06-13 23:41:10 -0400

    Updates

---
+++ precache-manifest.d9f8de29e47349a9f5db5cf2a999c0fc.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9eec2536a3af58b0ccbb18cc1709d5",
+    "url": "/old-site/index.html"
+  },
+  {
+    "revision": "cfc98e1f3bfe4ec2be27",
+    "url": "/old-site/static/css/2.163fc869.chunk.css"
+  },
+  {
+    "revision": "024e29e893c54a4adf2f",
+    "url": "/old-site/static/css/main.6d5fde23.chunk.css"
+  },
+  {
+    "revision": "cfc98e1f3bfe4ec2be27",
+    "url": "/old-site/static/js/2.c4798b65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/old-site/static/js/2.c4798b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024e29e893c54a4adf2f",
+    "url": "/old-site/static/js/main.3d6f5bcb.chunk.js"
+  },
+  {
+    "revision": "cb7e5761db9dadc1fab0",
+    "url": "/old-site/static/js/runtime-main.d6982769.js"
+  },
+  {
+    "revision": "af805024eee0a91dc28d1ef157b9ee66",
+    "url": "/old-site/static/media/D3.af805024.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/old-site/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/old-site/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/old-site/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "3cfd72f27a373bcc3ade68de1532c279",
+    "url": "/old-site/static/media/MUI.3cfd72f2.png"
+  },
+  {
+    "revision": "3a6ecf292206c91a0c00cbb93989c6df",
+    "url": "/old-site/static/media/React.3a6ecf29.png"
+  },
+  {
+    "revision": "d9cdf13c5fc0ab50040089d7411a0846",
+    "url": "/old-site/static/media/Redux.d9cdf13c.png"
+  },
+  {
+    "revision": "6c52bf15c935327159985a34cae8595b",
+    "url": "/old-site/static/media/gaurav_tamhan_resume_2020_updated.6c52bf15.pdf"
+  },
+  {
+    "revision": "5eb4066a794115751fe995b083a8c5d7",
+    "url": "/old-site/static/media/hero.5eb4066a.png"
+  },
+  {
+    "revision": "4da6ed9ddf5fccf0f0ed58dfd0df501d",
+    "url": "/old-site/static/media/image-1.4da6ed9d.png"
+  },
+  {
+    "revision": "f590bcbcc15ea0b56243027bcc544c14",
+    "url": "/old-site/static/media/image-2.f590bcbc.png"
+  },
+  {
+    "revision": "5aec5aca4e99c71dfbf5146dbca50806",
+    "url": "/old-site/static/media/image-3.5aec5aca.png"
+  },
+  {
+    "revision": "b8c98713318895320714efc6a7ac9bf3",
+    "url": "/old-site/static/media/side-menu.b8c98713.mp4"
+  },
+  {
+    "revision": "8d9fed02b0d07d9f7c4068f1c36622e6",
+    "url": "/old-site/static/media/theme.8d9fed02.mp4"
+  },
+  {
+    "revision": "132b241151c54db0b92ee607a19c1581",
+    "url": "/old-site/static/media/tiles.132b2411.mp4"
+  },
+  {
+    "revision": "e58b697db07740cc04b604469a0e1066",
+    "url": "/old-site/static/media/trainer-app.e58b697d.mp4"
+  }
+]);
