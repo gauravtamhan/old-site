c17625747a9dbd337c09a625e24208a804904572
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2020-06-29 15:45:11 -0400

    Removed Google Analytics tracker

--- src/index.js
+++ src/index.js
@@ -11,25 +11,30 @@ class App extends React.PureComponent {
         super(props);
         this.state = {
             initialized: false,
-        }
+        };
     }
 
     componentDidMount() {
-        ReactGA.initialize(process.env.REACT_APP_GA_TRACKER_ID)
-        this.setState({ initialized: true })
+        // ReactGA.initialize(process.env.REACT_APP_GA_TRACKER_ID)
+        this.setState({ initialized: true });
     }
 
     render() {
         const { initialized } = this.state;
 
-        return initialized && <Routes />
+        return initialized && <Routes />;
     }
 }
 
 // kick off the polyfill!
 smoothscroll.polyfill();
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
